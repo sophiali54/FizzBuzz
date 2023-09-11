deef5f6452fc4890171b538e802badcc31d272ef
Sophia Li <sophiali@Sophias-MacBook-Pro.local>
2023-09-11 16:21:43 -0400

    Task 3...

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(100);
+        // can't hand in anything, have to abide arg
+        String[] x = new String[] {};
+        FizzBuzz.main(x);
+        // FizzBuzz.main(null);
+    }
+}
+
---
+++ src/Multiples.java
@@ -0,0 +1,8 @@
+public class Multiples {
+    public static void main(String[] args) {
+
+        // declare variables
+         int x = 1000;
+        System.out.println((x / 5) + (x / 3));
+    }
+}
