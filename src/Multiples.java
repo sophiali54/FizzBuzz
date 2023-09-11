f4513d9c14ad95abcc38e8c41adb3cf15b847d48
Sophia Li <sophiali@Sophias-MacBook-Pro.local>
2023-09-11 16:24:01 -0400

    Task 3...

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,6 +1,8 @@
 public class Multiples {
     public static void main(String[] args) {
 
+        // testing
+
         // declare variables
          int x = 1000;
         System.out.println((x / 5) + (x / 3));
